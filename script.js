e7617f914f19124bd8522bb6d2dfe387b58ff15f
Danijel <dklobucar7@gmail.com>
2023-11-30 15:31:35 +0100

    Added animation

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const btnPlus = document.querySelector(".plus__button");
 const btnMinus = document.querySelector(".minus__button");
 const nameEl = document.querySelector(".app__name");
 const refreshEl = document.querySelector(".app__icon");
+const btnPlusIconEl = document.querySelector(".plus__button__icon");
+const btnMinusIconEl = document.querySelector(".minus__button__icon");
 
 let count = 1;
 
 btnPlus.addEventListener("click", function () {
   if (count < 5) {
+    nameEl.innerHTML = "Fancy <br />Counter";
     count = count + 1;
     counterEL.textContent = count;
+
+    // Clear the previous animation before applying a new one
+    btnPlusIconEl.style.animation = "none";
+    counterEL.style.animation = "none";
+
+    // Trigger the animation
+    void btnPlusIconEl.offsetWidth; // This line is used to force a reflow, enabling the animation to restart
+    void counterEL.offsetWidth;
+    btnPlusIconEl.style.animation = "make-bigger 0.5s";
+    counterEL.style.animation = "make-bigger 0.5s";
   } else {
     nameEl.innerHTML =
       'LIMIT! BUY <br /><span class="u-bolder u-accent">PRO</span> FOR > 5';
@@ -20,8 +33,19 @@ btnMinus.addEventListener("click", function () {
   if (count <= 0) {
     count = count;
   } else {
+    nameEl.innerHTML = "Fancy <br />Counter";
     count = count - 1;
     counterEL.textContent = count;
+
+    // Clear the previous animation before applying a new one
+    btnMinusIconEl.style.animation = "none";
+    counterEL.style.animation = "none";
+
+    // Trigger the animation
+    void btnMinusIconEl.offsetWidth; // This line is used to force a reflow, enabling the animation to restart
+    void counterEL.offsetWidth;
+    btnMinusIconEl.style.animation = "make-bigger 0.5s";
+    counterEL.style.animation = "make-bigger 0.5s";
   }
 });
 
@@ -29,4 +53,11 @@ refreshEl.addEventListener("click", function () {
   counterEL.textContent = "1";
   count = 1;
   nameEl.innerHTML = "Fancy <br />Counter";
+
+  // Clear the previous animation before applying a new one
+  refreshEl.style.animation = "none";
+
+  // Trigger the animation
+  void refreshEl.offsetWidth; // This line is used to force a reflow, enabling the animation to restart
+  refreshEl.style.animation = "make-bigger 0.2s";
 });
